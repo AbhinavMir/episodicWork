e3df3da688802e666c9fd81fc9f73c234c5d3378
Abhinav Srivastava <atg271@gmail.com>
2024-03-14 15:04:25 -0400

    wip

--- src/app/story/[storyurl]/page.tsx
+++ src/app/story/[storyurl]/page.tsx
@@ -12,16 +12,54 @@ interface Chapter {
   chapter_number: number;
 }
 
+
+const fetchStoryIdFromUrl = async (storyUrl: string): Promise<number | null> => {
+  let { data, error } = await supabase
+    .from('stories')
+    .select('story_id')
+    .eq('storyurl', storyUrl)
+    .single()
+
+  console.log(data);
+  console.log(error);
+  if (error) throw new Error(error.message)
+  return data ? data.story_id : null
+}
+
+const fetchChaptersByStoryId = async (storyId: number): Promise<any[]> => {
+  let { data, error } = await supabase
+    .from('chapters')
+    .select('*')
+    .eq('story_id', storyId)
+
+  if (error) throw new Error(error.message)
+  return data as any[]; // Add type assertion here
+}
+
+
+
 export default function Page({ params }: { params: { storyurl: string } }) {
   const [chapters, setChapters] = useState<Chapter[]>([]);
   const [loading, setLoading] = useState(true);
 
+  
   useEffect(() => {
-    async function getChapters() {}
+    async function getChapters() {
+      const storyId = await fetchStoryIdFromUrl(params.storyurl);
+      console.log(storyId);
+      if (!storyId) {
+        setLoading(false);
+        return;
+      }
+      const chapters = await fetchChaptersByStoryId(storyId);
+      setChapters(chapters);
+      setLoading(false);
+    }
 
     getChapters();
   }, [params.storyurl]);
 
+  console.log(chapters);
   return (
     <div>
       <Navbar />
